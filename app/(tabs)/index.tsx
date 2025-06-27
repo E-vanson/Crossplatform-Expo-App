3e735a9d152ce5ca491f95a26d9412ee80890af8
Grimm <evansonkamau233@gmail.com>
2025-06-27 10:13:24 +0300

    Feat: Sticker can be selected and scaled

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,39 +2,94 @@ import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Entry Point Of The App
 
 const placeHolderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
-  const [selectedImage, setSelectedImage]= useState<string | undefined>(undefined)
-  const pickImageAsync = async ()=>{
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setisModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<
+    ImageSourcePropType | undefined
+  >(undefined);
+
+  const onModalClose = () => {
+    setisModalVisible(false);
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setisModalVisible(true);
+  };
+
+  const onSaveImageAsync = () => {};
+
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
     });
 
-    if(!result.canceled){
-      setSelectedImage(result.assets[0].uri)
-      console.log(result)
-    }else{
-      alert("You didn't select any image.")
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+      console.log(result);
+    } else {
+      alert("You didn't select any image.");
     }
-  }
+  };
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={ selectedImage || placeHolderImage}/>
-      </View>          
-      <View style={styles.footerContainer}>
-        <Button
-        onPress={pickImageAsync}
-        label="Choose A Photo" theme="primary"/>
-        <Button label="Use This Photo"/>
+        <ImageViewer imgSource={selectedImage || placeHolderImage} />
+        {pickedEmoji && (<EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>)}
       </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            onPress={pickImageAsync}
+            label="Choose A Photo"
+            theme="primary"
+          />
+          <Button
+            label="Use This Photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
+    </GestureHandlerRootView>
+    
   );
 }
 
@@ -42,13 +97,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    alignItems: "center"
+    alignItems: "center",
   },
-  imageContainer:{
-    flex: 1
+  imageContainer: {
+    flex: 1,
   },
-  footerContainer:{
+  footerContainer: {
     flex: 1 / 3,
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
 });
